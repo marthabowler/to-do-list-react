e4c1ef8bb098c906878eb2b171e9d6f8dbccdbbe
Martha Bowler <mbowler133@gmail.com>
2021-11-30 16:19:36 +0000

    Add inputs to add new to-do

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import Header from "./components/header";
 import ToDoItems from "./components/to-do-items";
 import "./App.css";
+import InsertToDoItem from "./components/input-form";
 const axios = require("axios").default;
 
 function App(): JSX.Element {
   const [toDoState, setToDo] = useState([]);
+  const [addNewToDo, setNewToDo] = useState("");
+  const [addNewDueDate, setNewDueDate] = useState("");
 
   const loadData = async () => {
     const resp = await fetch("http://localhost:4000/items/");
@@ -26,6 +29,12 @@ function App(): JSX.Element {
     <>
       <Header />
       <br />
+      <InsertToDoItem
+        newDueDate={addNewDueDate}
+        newTask={addNewToDo}
+        setNewDueDate={setNewDueDate}
+        setNewTask={setNewToDo}
+      />
       <ToDoItems toDoAllItems={toDoState} deleteToDo={deleteToDo} />
     </>
   );
---
+++ src/components/input-form.tsx
@@ -0,0 +1,33 @@
+interface InsertToDoItemProps {
+  setNewDueDate: (input: string) => void;
+  setNewTask: (input: string) => void;
+  newTask: string;
+  newDueDate: string;
+}
+export default function InsertToDoItem(
+  props: InsertToDoItemProps
+): JSX.Element {
+  return (
+    <>
+      <div className="insert">
+        <h1>Insert new task</h1>
+        <input
+          placeholder="Task"
+          value={props.newTask}
+          onChange={(e) => {
+            props.setNewTask(e.target.value);
+          }}
+        />
+        <input
+          placeholder="Due Date"
+          value={props.newDueDate}
+          onChange={(e) => {
+            props.setNewDueDate(e.target.value);
+          }}
+        />
+        <button>Add task</button>
+      </div>
+      <br />
+    </>
+  );
+}
